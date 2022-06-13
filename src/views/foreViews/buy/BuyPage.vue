56cf8ee2d5a6203e44b3e7108b0f507547522b19
xy <xy1392055037@outlook.com>
2022-06-13 10:20:33 +0800

    提交购买详情

--- src/views/foreViews/buy/BuyPage.vue
+++ src/views/foreViews/buy/BuyPage.vue
@@ -12,14 +12,37 @@
           <el-row class="sku-desc padding-left">{{this.skuInfoList.sku_desc}}</el-row>
           <el-row class="price padding-left font-size">{{this.skuInfoList.price}}元</el-row>
           <el-row><div style="border-bottom: 1px solid #e0e0e0;margin:20px 0 20px 12px;"></div></el-row>
-          <el-row class="address padding-left">地址</el-row>
+          <el-row class="address padding-left">
+            <div class="el-icon-location"></div>
+            <span>北京</span>
+            <span style="margin-left: 20px;">北京市</span>
+            <span style="margin-left: 20px;">海淀区</span>
+            <span style="margin-left: 20px;">清河街道</span>
+            <el-button type="text" class="update" @click="dialogVisible = true">修改</el-button>
+            <el-dialog
+              title="提示"
+              :visible.sync="dialogVisible"
+              width="40%">
+              <span>这是一段信息</span>
+              <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+              </span>
+            </el-dialog>
+
+            <div style="color: #ff6700;margin-top: 10px;">{{this.haveStore}}</div>
+          </el-row>
           <el-row v-if="this.skuInfoList.sku_type=='手机'" class="font-size padding-left chosePro">选择版本</el-row>
           <el-row v-if="this.skuInfoList.sku_type=='手机'" class="version">
-            
+            <el-radio-group v-for="(item,index) in versionList" :key="index" v-model="versionBase">
+              <el-radio-button class="versionRadio" :label="index">{{item}}</el-radio-button>
+            </el-radio-group>
           </el-row>
           <el-row v-if="this.skuInfoList.sku_type=='手机'" class="font-size padding-left chosePro">选择颜色</el-row>
           <el-row v-if="this.skuInfoList.sku_type=='手机'" class="version">
-
+            <el-radio-group  class="versionRadio" v-for="(item,index) in colorList" :key="index" v-model="colorBase">
+              <el-radio-button :label="index">{{item}}</el-radio-button>
+            </el-radio-group>
           </el-row>
           <el-row v-if="this.skuInfoList.sku_type=='电脑'" class="font-size padding-left chosePro">选择配置</el-row>
           <el-row v-if="this.skuInfoList.sku_type=='电脑'" class="version">
@@ -31,7 +54,13 @@
             
           </el-row>
 
-          <el-row class="info padding-left">购买详情</el-row>
+          <el-row class="info padding-left">
+            <div class="desc">
+              {{this.skuInfoList.sku_name}}&nbsp;&nbsp;&nbsp;&nbsp;{{this.versionList[versionBase]}}&nbsp;&nbsp;&nbsp;&nbsp;{{this.colorList[colorBase]}}
+              <span style="margin-left:400px">{{this.skuInfoList.price}}元</span>
+            </div>
+            <div class="sum">总计：{{this.skuInfoList.price}}元</div>
+          </el-row>
           <el-row class="padding-left cartButton">
             <el-button class="buttonCart" type="primary">加入购物车</el-button>
             <el-button class="buyButton">立即购买</el-button>
@@ -55,6 +84,10 @@ export default{
       cpList:[],
       seriesList:[],
       imgUrl:'',
+      versionBase:0,
+      colorBase:0,
+      haveStore:'',
+      dialogVisible: false
     }
   },
   methods: {
@@ -81,6 +114,7 @@ export default{
       this.seriesList = this.parameterList.parameter_series.split(',');
       this.skuInfoList = res.data[0].skuInfo[0];
       this.imgUrl = 'http://localhost:8081/' + this.skuInfoList.img;
+      this.haveStore = res.data[0].skuInfo[0].store > 0 ? '有现货' : '无现货';
     })
   },
   components:{
@@ -90,6 +124,10 @@ export default{
 </script>
 
 <style>
+  .update{
+    color: #ff6700 !important;
+    margin-left: 20px;
+  }
   .border{
     border: 1px solid red;
   }
@@ -153,7 +191,17 @@ export default{
     width: 220px;
     height: 50px;
   }
-  
-  
+  .versionRadio{
+    margin-right: 40px;
+  }
+  .sum{
+    margin-top: 30px;
+    color: #ff6700;
+    font-size: 25px;
+  }
+  .desc{
+    color:#616161;
+    font-size: 14px;
+  }
  
 </style>
