5bec2940a3ee446bcb7e434f3c1aef3c4525e330
xy <xy1392055037@outlook.com>
2022-06-16 10:08:32 +0800

    提交收货地址

--- src/views/foreViews/address/AddressInfo.vue
+++ src/views/foreViews/address/AddressInfo.vue
@@ -5,7 +5,7 @@
         <el-dialog
           title="新增地址"
           :visible.sync="dialogVisible"
-          width="40%">
+          width="40%" append-to-body>
             <el-form ref="addData" :model="addData" label-width="80px">
               <el-form-item label="收货人">
                  <template slot-scope="scope">
@@ -30,14 +30,10 @@
               </el-form-item>
               <el-form-item >
                 <template slot-scope="scope">
-                  <el-checkbox
-                      v-model= addData.address_status
-                      :true-label = "0"
-                      :false-label = "1"
-                      :disabled = "checkStatus"
-                      @change="changetest">
+                  <el-radio
+                      v-model= "addData.address_status" label="0">
                     设为默认
-                  </el-checkbox>
+                  </el-radio>
                 </template>
               </el-form-item>
             </el-form>
@@ -53,7 +49,7 @@
           <el-table-column type="index" label="序号"></el-table-column>
           <el-table-column prop="address_id"  label="地址id"  v-if="false"></el-table-column>
           <el-table-column prop="derive_name" label="收货人"></el-table-column>
-          <el-table-column label="收货地址" prop="address_level1" align="center"></el-table-column>
+          <el-table-column label="收货地址" prop="address" align="center"></el-table-column>
           <el-table-column prop="address_info" label="详细地址" align="center"></el-table-column>
            <el-table-column
               prop="address_status"
@@ -70,10 +66,51 @@
               <el-button
                   size="small"
                   icon="el-icon-edit"
-                  @click="editAddress(scope.$index,scope.row)">编辑</el-button>
+                  @click="editAddress(scope.row)">编辑</el-button>
+
+                  <el-dialog
+          title="编辑地址"
+          :visible.sync="editAddressVisible"
+          width="40%" append-to-body>
+            <el-form ref="editData" :model="editData" label-width="80px">
+              <el-form-item label="收货人">
+                 <template slot-scope="scope">
+                    <el-input v-model="editData.derive_name" placeholder="请输入收货人"></el-input>
+                 </template>
+              </el-form-item>
+              <el-form-item label="收货地址">
+                  <el-cascader 
+                      placeholder="请选择"
+                      @change="handleAddress"
+                      v-model="selectEditOptions"
+                      :options="options1"
+                      filterable
+                  ></el-cascader>
+              </el-form-item>
+              <el-form-item label="详细地址">
+                <template slot-scope="scope">
+                  <el-input v-model="editData.address_info" placeholder="请输入详细地址"></el-input>
+                </template>
+              </el-form-item>
+              <el-form-item >
+                <template slot-scope="scope">
+                  <el-radio
+                      v-model="editData.address_status" label="0">
+                    设为默认
+                  </el-radio>
+                </template>
+              </el-form-item>
+            </el-form>
+          <span slot="footer">
+            <el-button @click="editAddressVisible = false">取 消</el-button>
+            <el-button type="primary" @click="updateAddress('editData')">确 定</el-button>
+          </span>
+        </el-dialog>
+
+
               <el-button
                   size="small"
-                  icon="el-icon-delete" @click="deleteAddress">删除</el-button>
+                  icon="el-icon-delete" @click="deleteAddress(scope.row)">删除</el-button>
             </template>
           </el-table-column>
       </el-table>
@@ -92,36 +129,25 @@ export default{
     return {
       addData:{
         user_id:window.sessionStorage.getItem('user_id'),
-        address_status:1,
+        address_status:'1',
       },
+      editData:{},
       addressData: [],
       addressEdit: [],
       checkStatus: false,
       address_load:'',
       selectedOptions: [],
+      selectEditOptions: [],
       options: regionData,
-      dialogVisible: false
+      options1: regionData,
+      dialogVisible: false,
+      editAddressVisible: false,
     }
   },
   created() {
     this.loadAddressData();
-    this.check();
   },
   methods: {
-    check(){
-      this.axios.get(
-        '/address/queryStatus',
-        {
-          params: {
-            user_id:window.sessionStorage.getItem('user_id')
-          }
-        }
-      ).then(res => {
-        if(res.data > 0){
-          this.checkStatus = true;
-        }
-      })
-    },
     loadAddressData() {
       this.axios.get(
         '/address/queryAddress',
@@ -133,7 +159,7 @@ export default{
       ).then(res => {
         this.addressData = res.data;
         this.addressData.forEach(item => {
-          item.address_level1 = CodeToText[item.address_level1] + "/" + CodeToText[item.address_level2] + "/" + CodeToText[item.address_level3];
+          item.address = CodeToText[item.address_level1] + "/" + CodeToText[item.address_level2] + "/" + CodeToText[item.address_level3];
         });
       });
     },
@@ -164,26 +190,89 @@ export default{
           }
       })
     },
+    updateAddress(formName){
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.axios.get(
+            '/address/update',
+            {
+              params:this.editData
+            }
+          ).then(res=>{
+            if(res.data == '修改成功'){
+                this.$message({
+                    message: '修改成功',
+                    type: 'success'
+                });
+              } else {
+                this.$message({
+                    message: '修改成功',
+                    type: 'error'
+                });
+              }
+              this.editAddressVisible = false;
+              this.loadAddressData();
+          })
+        }
+      
+      })
+    },
     handleAddressNode() {
       this.addData.address_level1 = this.selectedOptions[0];
       this.addData.address_level2 = this.selectedOptions[1];
       this.addData.address_level3 = this.selectedOptions[2];
     },
-    editAddress(index,value) {
-
+    handleAddress(){
+      this.editData.address_level1 = this.selectEditOptions[0];
+      this.editData.address_level2 = this.selectEditOptions[1];
+      this.editData.address_level3 = this.selectEditOptions[2];
+    
     },
-    changetest(value) {
+    editAddress(value) {
+      this.editAddressVisible = true;
+      this.axios.get(
+        '/address/selectById',
+        {
+          params:{
+            address_id:value.address_id
+          }
+        }
+      ).then(res=>{
+        this.editData = res.data[0];
+        this.editData.address_status = res.data[0].address_status == 0 ? '0' : '1';
+        this.selectEditOptions = [this.editData.address_level1,this.editData.address_level2,this.editData.address_level3];
+        console.log(res.data[0]);
+        console.log(this.selectEditOptions);
+      })
     },
-    deleteAddress() {
+    deleteAddress(value) {
        this.$confirm('此操作将永久删除该地址, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          
-            //删除地址
-          }
-        ).catch(() => {
+            this.axios.get(
+              '/address/deleteById',
+              {
+                params:{
+                  address_id:value.address_id
+                }
+              }
+            ).then(res=>{
+              if(res.data == '删除成功'){
+                this.$message({
+                      message: '删除成功',
+                      type: 'success'
+                  });
+              } else {
+                  this.$message({
+                      message: '删除失败',
+                      type: 'error'
+                  });
+              }
+              this.loadAddressData();
+            })
+          }).catch(() => {
         })
     }
   }
