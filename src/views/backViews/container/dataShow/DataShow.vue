c083d98b74ad44d6e02effde5d929424d9f5f9bc
xy <xy1392055037@outlook.com>
2022-07-04 15:58:24 +0800

    提交数据显示

--- src/views/backViews/container/dataShow/DataShow.vue
+++ src/views/backViews/container/dataShow/DataShow.vue
@@ -1,5 +1,10 @@
 <template>
   <div style="width:100%;height:100%;">
+    <el-button id="analys"
+      type="primary"
+      @click="openFullScreen2" v-if="false">
+      分析数据
+    </el-button>
     <div class="main">
       <el-card class="elcard1" shadow="hover">用户人数：{{count}}人</el-card>
       <el-card class="elcard2" shadow="hover">销售总额：{{sumPrice}}元</el-card>
@@ -24,6 +29,8 @@
           option3:{},
           count:0,
           sumPrice:0,
+          week:[],
+          weekcount:[]
         }
       },
       methods: {
@@ -35,133 +42,184 @@
             this.sumPrice = res.data.sumPrice;
           })
         },
-        getDataList(){
-          var categoryData =[];
-          var time = new Date();
-          //7天前
-          var time1 = new Date(time.getTime() - 7 * 24 * 60 * 60 * 1000);
-          //YY-MM-DD
-          var time2 = time1.getFullYear() + "-" + (time1.getMonth() + 1) + "-" + time1.getDate();
-          for (var i = 0; i <= 7; i++) {
-            var time3 = new Date(time1.getTime() + i * 24 * 60 * 60 * 1000);
-            var time4 = (time3.getMonth() + 1) + "-" + time3.getDate();
-            categoryData.push(time4);
-          }
-          return categoryData;
+        openFullScreen2() {
+          const loading = this.$loading({
+            lock: true,
+            text: 'Loading',
+            spinner: 'el-icon-loading',
+            background: 'rgba(0, 0, 0, 0.7)'
+          });
+          this.axios.get(
+            '/sqoop/execLinux'
+          ).then(res=>{
+            if(res.data==='success'){
+              loading.close();
+            }else{
+              loading.close();
+              this.$message.error('执行失败');
+            }
+          })
+          
         },
         load(){
-          var chartDom = document.getElementById('main');
-          var chartDom1 = document.getElementById('main1');
-          var chartDom2 = document.getElementById('main2');
-          var myChart = this.echarts.init(chartDom);
-          var myChart1 = this.echarts.init(chartDom1);
-          var myChart2 = this.echarts.init(chartDom2);
-          this.option1 = {
-            title: {
-              left: 'center',
-              bottom:'bottom',
-              text: '最近7日销售分析'
-            },
-            toolbox: {
-              show: true,
-              feature: {
-                dataZoom: {
-                  yAxisIndex: 'none'
-                },
-                dataView: { readOnly: false },
-                magicType: { type: ['line', 'bar'] },
-                restore: {},
-                saveAsImage: {}
-              }
-            },
-            xAxis: {
-              type: 'category',
-              name: "日期",
-              data: this.getDataList(),
+          this.axios.get(
+            '/sqoop/getTypePrice'
+          ).then(res=>{
+            let type = [];
+            let typeDate = [];
+            for(let key in res.data[0]){
+              type.push(key);
+              typeDate.push(res.data[0][key]);
+            }
+            var chartDom1 = document.getElementById('main1');
+            var myChart1 = this.echarts.init(chartDom1);
+            this.option2 = {
+              title: {
+                left: 'center',
+                bottom:'bottom',
+                text: '产品类型总销售金额'
+              },
+              xAxis: {
+                type: 'category',
+                data: type
+              },
+              yAxis: {
+                type: 'value'
+              },
               
-            },
-            yAxis: {
-              type: 'value'
-            },
-            series: [
-              {
-                data: [150, 230, 224, 218, 135, 147, 260],
-                type: 'line'
-              }
-            ]
-          };
-          this.option2 = {
-            title: {
-              left: 'center',
-              bottom:'bottom',
-              text: '产品类型年销售情况'
-            },
-            legend: {},
-            tooltip: {},
-            dataset: {
-              dimensions: ['product', '2015', '2016', '2017'],
-              source: [
-                { product: '手机', 2015: 43, 2016: 85, 2017: 93 },
-                { product: '手表', 2015: 83, 2016: 73, 2017: 55 },
-                { product: '电脑', 2015: 86, 2016: 65, 2017: 82 }
+              series: [
+                {
+                  data: typeDate,
+                  type: 'bar',
+                  itemStyle:{
+                    normal:{
+                      label:{
+                        formatter:"{c}" + "元",
+                        show:true,
+                        position:'top'
+                      }
+                    }
+                  },
+                }
               ]
-            },
-            xAxis: { type: 'category' },
-            yAxis: {},
-            // Declare several bar series, each will be mapped
-            // to a column of dataset.source by default.
-            series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
-          };
-          this.option3 = {
-            title: {
-              left: 'center',
-              bottom:'bottom',
-              text: '产品类型总购买情况'
-            },
-             tooltip: {
-              trigger: 'item',
-              formatter: '{a} <br/>{b} : {c} ({d}%)'
-            },
-            legend: {
-              top: '5%',
-              left: 'center'
-            },
-            series: [
-              {
-                name: '购买占比',
-                type: 'pie',
-                radius: ['40%', '70%'],
-                avoidLabelOverlap: false,
-                label: {
-                  show: false,
-                  position: 'center'
-                },
-                emphasis: {
+            };
+            
+            myChart1.setOption(this.option2);
+            })
+          
+        },
+        loadNum(){
+          this.axios.get(
+            '/sqoop/getTypeNum'
+          ).then(res=>{
+          var chartDom2 = document.getElementById('main2');
+          var myChart2 = this.echarts.init(chartDom2);
+            this.option3 = {
+              title: {
+                left: 'center',
+                bottom:'bottom',
+                text: '产品类型总购买情况'
+              },
+              tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+              },
+              legend: {
+                top: '5%',
+                left: 'center'
+              },
+              series: [
+                {
+                  name: '购买占比',
+                  type: 'pie',
+                  radius: ['40%', '70%'],
+                  avoidLabelOverlap: false,
                   label: {
-                    show: true,
-                    fontSize: '40',
-                    fontWeight: 'bold'
-                  }
+                    show: false,
+                    position: 'center'
+                  },
+                  emphasis: {
+                    label: {
+                      show: true,
+                      fontSize: '40',
+                      fontWeight: 'bold'
+                    }
+                  },
+                  labelLine: {
+                    show: false
+                  },
+                  data: res.data
+                }
+              ]
+            }
+            myChart2.setOption(this.option3);
+          })
+        },
+        loadDate(){
+          this.axios.get(
+            '/sqoop/getTypeDate'
+          ).then(res=>{
+            let week1 = [];
+            let weekcount1 = [];
+            for(let key in res.data[0]){
+              week1.push(key.substring(5,10));
+              weekcount1.push(res.data[0][key]);
+            }
+            var chartDom = document.getElementById('main');
+            var myChart = this.echarts.init(chartDom);
+            this.option1 = {
+              title: {
+                left: 'center',
+                bottom:'bottom',
+                text: '最近7日销售分析'
+              },
+              toolbox: {
+                show: true,
+                feature: {
+                  dataZoom: {
+                    yAxisIndex: 'none'
+                  },
+                  dataView: { readOnly: false },
+                  magicType: { type: ['line', 'bar'] },
+                  restore: {},
+                  saveAsImage: {}
+                }
+              },
+              xAxis: {
+                type: 'category',
+                name: "日期",
+                data: week1,
+                axisLabel:{
+                  interval: 0,//X轴信息全部展示
                 },
-                labelLine: {
-                  show: false
-                },
-                data: [
-                  { value: 1048, name: '手机' },
-                  { value: 735, name: '电脑' },
-                  { value: 580, name: '手表' }
-                ]
-              }
-            ]
-          }
-          myChart.setOption(this.option1);
-          myChart1.setOption(this.option2);
-          myChart2.setOption(this.option3);
+                axisTick: {
+                  alignWithLabel: true
+                }
+              },
+              yAxis: {
+                type: 'value'
+              },
+              tooltip : {
+                trigger: 'axis',
+                
+              },
+              series: [
+                {
+                  data: weekcount1,
+                  type: 'line'
+                }
+              ]
+            };
+            myChart.setOption(this.option1);
+          })
         }
       },
-      mounted() {
+      
+      mounted() { 
         this.load();
         this.loadCount();
+        this.loadDate();
+        this.loadNum();
       },
   }
 </script>
@@ -171,7 +229,7 @@
     border: 1px red solid;
   }
   .main{
-    height: 25%;
+    height: 20%;
   }
   .myecharts{
     height: 75%;
@@ -185,7 +243,7 @@
     display: inline-block;
     padding-top: 35px;
     position: relative;
-    top: 20%;
+    top: 10%;
     left: 20%;
   }
   .elcard2{
@@ -197,7 +255,7 @@
     display: inline-block;
     padding-top: 35px;
     position: relative;
-    top: 20%;
+    top: 10%;
     left: 40%;
   }
   .echarts{
